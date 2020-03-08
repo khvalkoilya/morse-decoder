26698e28afeac543b8d06f1c66f99d6ee3f8182d
khvalkoilya <khvalko.ilya@gmail.com>
2020-03-09 00:36:58 +0300

    fead: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let str="", start=0, end=10, element, array=[];
+    if(expr.length%10!=0) throw new Error;
+    let lengthOfStr=expr.length/10;
+    for(let i=1;i<=lengthOfStr;i++,start+=10,end+=10) {
+        element=expr.slice(start,end);
+        if(element=="**********") str+=" ";
+        else {
+            while(element[0]!=1) element=element.slice(1);
+            let char="";
+            for(let j=0;j<element.length;j+=2)
+                if(element[j+1]=="0") char+=".";
+                else char+="-";
+                str+=MORSE_TABLE[char];
+        }
+    }
+    return str;
 }
+
 
 module.exports = {
     decode
